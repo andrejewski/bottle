1ce70aa149952aa585caf9a63fd78fd25cc401b5
Chris Andrejewski <chrisishereladies@gmail.com>
2013-03-30 15:10:49 -0400

    Got rid of trash text

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 var ip = '192.168.1.45';
-var port = 5555;
+var port = 5454;
 
 module.exports = {
     development: {
@@ -7,15 +7,15 @@ module.exports = {
       app: {
         name: 'Bottle'
       },
-      db: 'mongodb://localhost/bottle_dev',
+      db: '$YOUR DB HERE',
       facebook: {
           clientID: "APP_ID"
         , clientSecret: "APP_SECRET"
         , callbackURL: "http://"+ip+":"+port+"/auth/facebook/callback"
       },
       twitter: {
-          clientID: "hLxvHwTVcC2ehbMfXZcg"
-        , clientSecret: "8IkEl7GRkTOdCoy6gsk26dauLSD1BaDA1QvpW3o5w"
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
         , callbackURL: "http://"+ip+":"+port+"/auth/twitter/callback"
       },
       github: {
@@ -33,30 +33,6 @@ module.exports = {
 
     }
   , production: {
-      root: require('path').normalize(__dirname + '/..'),
-      app: {
-        name: 'Hurl'
-      },
-      db: 'mongodb://hurl.aws.af.cm/hurl_dev',
-      facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:8888/auth/facebook/callback"
-      },
-      twitter: {
-          clientID: "MEsnlKSjt4sBPwWSGt54A"
-        , clientSecret: "V2uThixOPG4qwJ1iD1QOlKEyvVZmgZq493ONtns3PuI"
-        , callbackURL: "http://hurl.aws.af.cm/auth/twitter/callback"
-      },
-      github: {
-          clientID: 'APP_ID'
-        , clientSecret: 'APP_SECRET'
-        , callbackURL: 'http://localhost:8888/auth/github/callback'
-      },
-      google: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:8888/auth/google/callback"
-      }
+    
     }
 }
